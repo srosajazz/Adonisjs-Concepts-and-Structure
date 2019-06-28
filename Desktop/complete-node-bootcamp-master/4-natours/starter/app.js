a64f28ea18adcc7819078e4ddcb977978b0d1652
Sergio Rosa <scarlosjazz@gmail.com>
2019-06-28 06:49:06 -0400

    API: Handling GET Requests

--- Desktop/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Desktop/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,15 +1,28 @@
+const fs = require('fs');
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+// app.post('/', (req, res) => {
+//   res.send('You can Post endpoint...');
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can Post endpoint...');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      results: tours.length,
+      tours
+    }
+  });
 });
 
 const port = 3001;
